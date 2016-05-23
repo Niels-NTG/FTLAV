2dd0800d1bfc86c548b613814a22c46e67c9e2db
Niels NTG <NTG999@gmail.com>
2016-05-23 09:07:55 +0200

    Proper import formatting of DataParser

--- src/main/java/net/ntg/ftl/parser/ParseCSV.java
+++ src/main/java/net/ntg/ftl/parser/ParseCSV.java
@@ -78,7 +78,7 @@ public class ParseCSV {
 		LinkedHashMap<String, String> newRow = new LinkedHashMap<>();
 		newRow.put("TIME", FTLAdventureVisualiser.fileChangedTimeStamp);
 		newRow.put("SHIP NAME", FTLAdventureVisualiser.gameState.getPlayerShipName());
-		newRow.put("SHIP TYPE", net.ntg.ftl.parser.DataParser.getFullShipType());
+		newRow.put("SHIP TYPE", DataParser.getFullShipType());
 		newRow.put("DIFFICULTY", FTLAdventureVisualiser.gameState.getDifficulty().toString());
 		newRow.put("AE CONTENT", FTLAdventureVisualiser.gameState.isDLCEnabled() ? "enabled" : "disabled");
 		// Location
@@ -86,16 +86,16 @@ public class ParseCSV {
 		newRow.put("SECTOR NUMBER", Integer.toString(sectorNumber + 1));
 		newRow.put("SECTOR TYPE", FTLAdventureVisualiser.sectorArray.get(sectorNumber).getType());
 		newRow.put("SECTOR TITLE", FTLAdventureVisualiser.sectorArray.get(sectorNumber).getTitle());
-		newRow.put("FLEET ADVANCEMENT", Integer.toString(net.ntg.ftl.parser.DataParser.getRebelFleetAdvancement()));
+		newRow.put("FLEET ADVANCEMENT", Integer.toString(DataParser.getRebelFleetAdvancement()));
 		// Log
 		newRow.put("TOTAL SHIPS DEFEATED", Integer.toString(FTLAdventureVisualiser.gameState.getTotalShipsDefeated()));
 		newRow.put("TOTAL SCRAP COLLECTED", Integer.toString(FTLAdventureVisualiser.gameState.getTotalScrapCollected()));
 		newRow.put("TOTAL CREW HIRED", Integer.toString(FTLAdventureVisualiser.gameState.getTotalCrewHired()));
-		newRow.put("SCORE", Integer.toString(net.ntg.ftl.parser.DataParser.getCurrentScore()));
+		newRow.put("SCORE", Integer.toString(DataParser.getCurrentScore()));
 		// Encounter
-		newRow.put("HAZARDS", net.ntg.ftl.parser.DataParser.getBeaconHazards());
+		newRow.put("HAZARDS", DataParser.getBeaconHazards());
 		newRow.put("EVENT TEXT", FTLAdventureVisualiser.gameState.getEncounter().getText());
-		newRow.put("STORE", net.ntg.ftl.parser.DataParser.getStoreListing());
+		newRow.put("STORE", DataParser.getStoreListing());
 		// Supplies
 		newRow.put("SCRAP", Integer.toString(FTLAdventureVisualiser.shipState.getScrapAmt()));
 		newRow.put("HULL", Integer.toString(FTLAdventureVisualiser.shipState.getHullAmt()));
@@ -103,9 +103,9 @@ public class ParseCSV {
 		newRow.put("DRONE PARTS", Integer.toString(FTLAdventureVisualiser.shipState.getDronePartsAmt()));
 		newRow.put("MISSILES", Integer.toString(FTLAdventureVisualiser.shipState.getMissilesAmt()));
 		newRow.put("CREW SIZE", Integer.toString(FTLAdventureVisualiser.playerCrewState.size()));
-		newRow.put("CARGO", net.ntg.ftl.parser.DataParser.getCargoListing());
-		newRow.put("AUGMENTS", net.ntg.ftl.parser.DataParser.getAugmentListing());
-		newRow.put("OXYGEN LEVEL", Integer.toString(net.ntg.ftl.parser.DataParser.getShipOxygenLevel()));
+		newRow.put("CARGO", DataParser.getCargoListing());
+		newRow.put("AUGMENTS", DataParser.getAugmentListing());
+		newRow.put("OXYGEN LEVEL", Integer.toString(DataParser.getShipOxygenLevel()));
 		// Systems
 		newRow.put("POWER CAPACITY", Integer.toString(FTLAdventureVisualiser.shipState.getReservePowerCapacity()));
 		newRow.put("SHIELD SYSTEM CAPACITY", Integer.toString(FTLAdventureVisualiser.shipState.getSystem(SavedGameParser.SystemType.SHIELDS).getCapacity()));
@@ -120,7 +120,7 @@ public class ParseCSV {
 		newRow.put("WEAPONS SYSTEM CAPACITY", Integer.toString(FTLAdventureVisualiser.shipState.getSystem(SavedGameParser.SystemType.WEAPONS).getCapacity()));
 		newRow.put("WEAPONS SYSTEM POWER", Integer.toString(FTLAdventureVisualiser.shipState.getSystem(SavedGameParser.SystemType.WEAPONS).getPower()));
 		newRow.put("WEAPONS SYSTEM DAMAGE", Integer.toString(FTLAdventureVisualiser.shipState.getSystem(SavedGameParser.SystemType.WEAPONS).getDamagedBars()));
-		for (int i = 0; i < net.ntg.ftl.parser.DataParser.getWeaponSlotCount(); i++) {
+		for (int i = 0; i < DataParser.getWeaponSlotCount(); i++) {
 			try {
 				newRow.put("WEAPON SLOT " + (i+1), FTLAdventureVisualiser.shipState.getWeaponList().get(i).getWeaponId().replaceAll("_"," "));
 			} catch (IndexOutOfBoundsException e) {}
@@ -128,7 +128,7 @@ public class ParseCSV {
 		newRow.put("DRONE CONTROL SYSTEM CAPACITY", Integer.toString(FTLAdventureVisualiser.shipState.getSystem(SavedGameParser.SystemType.DRONE_CTRL).getCapacity()));
 		newRow.put("DRONE CONTROL SYSTEM POWER", Integer.toString(FTLAdventureVisualiser.shipState.getSystem(SavedGameParser.SystemType.DRONE_CTRL).getPower()));
 		newRow.put("DRONE CONTROL SYSTEM DAMAGE", Integer.toString(FTLAdventureVisualiser.shipState.getSystem(SavedGameParser.SystemType.DRONE_CTRL).getDamagedBars()));
-		for (int i = 0; i < net.ntg.ftl.parser.DataParser.getDroneSlotCount(); i++) {
+		for (int i = 0; i < DataParser.getDroneSlotCount(); i++) {
 			try {
 				newRow.put("DRONE SLOT " + (i+1), FTLAdventureVisualiser.shipState.getDroneList().get(i).getDroneId().replaceAll("_"," "));
 			} catch (IndexOutOfBoundsException e) {}
@@ -167,7 +167,7 @@ public class ParseCSV {
 		for (int i = 0; i < FTLAdventureVisualiser.gameState.getTotalCrewHired(); i++) {
 			try {
 				newRow.put("CREW MEMBER " + (i+1) + " NAME", FTLAdventureVisualiser.playerCrewState.get(i).getName());
-				newRow.put("CREW MEMBER " + (i+1) + " SPECIES", net.ntg.ftl.parser.DataParser.getFullCrewType(i));
+				newRow.put("CREW MEMBER " + (i+1) + " SPECIES", DataParser.getFullCrewType(i));
 				newRow.put("CREW MEMBER " + (i+1) + " HEALTH", Integer.toString(FTLAdventureVisualiser.playerCrewState.get(i).getHealth()));
 				newRow.put("CREW MEMBER " + (i+1) + " PILOT SKILL", Integer.toString(FTLAdventureVisualiser.playerCrewState.get(i).getPilotSkill()));
 				newRow.put("CREW MEMBER " + (i+1) + " ENGINE SKILL", Integer.toString(FTLAdventureVisualiser.playerCrewState.get(i).getEngineSkill()));
